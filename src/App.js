78169fdbf494bffef3fce639ff9703544d9bbf3d
Ssellu <ssellu.lee@gmail.com>
2023-10-09 17:15:21 +0900

    Change BgColor

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
 import MainContent from "./components/MainContent";
 import { I18nextProvider } from "react-i18next";
@@ -40,11 +40,11 @@ const App = () => {
     };
   }, []); // End of Intersection Observer API
   return (
-    <HashRouter basename="/frozen-city">
+    <BrowserRouter basename={`${process.env.PUBLIC_URL}/frozen-city`}>
       <I18nextProvider i18n={i18n}>
         <MainContent />
       </I18nextProvider>
-    </HashRouter>
+    </BrowserRouter>
   );
 };
 
